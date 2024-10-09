51f9fd82e5416600def0c4021689de18cb6b08bb
fitronansori <fitronansori7@gmail.com>
2024-10-09 19:19:20 +0800

    Add ActivityCard component

---
+++ src/components/layouts/Activity/ActivityCard.tsx
@@ -0,0 +1,31 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ActivityType } from "@/constants/data";
+
+type ActivityCardProps = {
+  item: ActivityType;
+};
+
+const ActivityCard = ({ item }: ActivityCardProps) => {
+  return (
+    <Card className="w-[320px] h-[400px]">
+      <CardHeader>
+        <CardTitle className="line-clamp-2 cursor-pointer">
+          {item.title}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="line-clamp-3">{item.desc}</p>
+      </CardContent>
+      <CardFooter>
+        <img src={item.img} alt="" className="rounded-md cursor-pointer" />
+      </CardFooter>
+    </Card>
+  );
+};
+export default ActivityCard;
