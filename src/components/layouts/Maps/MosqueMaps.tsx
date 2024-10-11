1299e5e7e13e7c6e0b73c34670981895e6c6ac76
fitronansori <fitronansori7@gmail.com>
2024-10-11 14:26:18 +0800

    Add MosqueMaps component for displaying a mosque location on Google Maps

---
+++ src/components/layouts/Maps/MosqueMaps.tsx
@@ -0,0 +1,12 @@
+const MosqueMaps = () => {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3944.5784745990886!2d116.275013!3d-8.636402!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcdb7fbd39a41db%3A0xe0a45048d9e233ac!2sMasjid%20Nurul%20Iman%20Lendang%20Gocek!5e0!3m2!1sid!2sid!4v1728627838057!5m2!1sid!2sid"
+      className="w-full min-h-[450px]"
+      style={{ border: 0 }}
+      allowFullScreen
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"></iframe>
+  );
+};
+export default MosqueMaps;
